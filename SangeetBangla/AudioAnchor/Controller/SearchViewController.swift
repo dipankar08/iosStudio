89f2afe6359f2737841eadf9fa6e59d0674599f1
Dipankar Dutta <dip@Dipankar-Mac.local>
2018-01-17 01:50:24 +0000

    Sangeet Bangla - seach added

--- SangeetBangla/AudioAnchor/Controller/SearchViewController.swift
+++ SangeetBangla/AudioAnchor/Controller/SearchViewController.swift
@@ -10,14 +10,15 @@ import UIKit
 import AVKit
 import AVFoundation
 
-class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
-    
+class SearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating  {
+
     var albumPlaylist: [Album] = []
     let apiClient = APIClient()
     var currentIndex: Int = Int()
     @IBOutlet weak var tableView: UITableView!
+    var previousSerachText = ""
 
-    
+    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -29,7 +30,36 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
         self.tableView.dataSource = self;
         self.tableView.delegate = self;
         
-        apiClient.fetchAudioTracks { (results, errorMessage) in
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search album"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        searchController.searchBar.scopeButtonTitles = ["Album", "Songs", "Artist", "Offlines"]
+        searchController.searchBar.delegate = self
+        
+        apiClient.fetchAudioTracks(text: "?album_name=Nach") { (results, errorMessage) in
+            if let results = results{
+                self.albumPlaylist = results
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        if previousSerachText == searchText {
+            return
+        }
+        previousSerachText = searchText
+        apiClient.fetchAudioTracks(text: "?album_name="+searchText) { (results, errorMessage) in
             if let results = results{
                 self.albumPlaylist = results
                 self.tableView.reloadData()
@@ -77,6 +107,13 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
         }
     }
     
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    // MARK: - UISearchBar Delegate
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        filterContentForSearchText(searchBar.text!, scope: searchBar.scopeButtonTitles![selectedScope])
+    }
 }
 
 
--- SangeetBangla/AudioAnchor/Networking/APIClient.swift
+++ SangeetBangla/AudioAnchor/Networking/APIClient.swift
@@ -22,15 +22,18 @@ class APIClient {
     var dataTask: URLSessionDataTask?
     let tracksEndpoint = "http://simplestore.dipankar.co.in/api/bengalimusic1"//https://s3-us-west-2.amazonaws.com/anchor-website/challenges/bsb.json"
     
-    func fetchAudioTracks(completion: @escaping JSONResponse) {
+    func fetchAudioTracks(text:String, completion: @escaping JSONResponse) {
          dataTask?.cancel()
-        
-        guard let url = NSURL(string: tracksEndpoint) else {
+        self.albums = []
+        var nowtracksEndpoint = tracksEndpoint + text
+        print(nowtracksEndpoint);
+        guard let url = NSURL(string: nowtracksEndpoint) else {
             print("This isn't working")
             return }
         var request = NSMutableURLRequest(url: url as URL)
         request.httpMethod = "GET"
         request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
+        print(request.url)
 
         
         dataTask = URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
@@ -88,41 +91,6 @@ class APIClient {
             albums.append(newAlbum)
         }
     }
-    /*
-    func serializeJSON1(_ data: Data) {
-        var response: JSONDictionary?
-        
-        do {
-            response = try JSONSerialization.jsonObject(with: data, options: []) as? JSONDictionary
-        } catch let parseError as NSError {
-            errorMessage += "JSONSerialization error: \(parseError.localizedDescription)\n"
-            return
-        }
-        
-        guard let trackArray = response!["tracks"] as? [Any] else {
-            errorMessage += "Dictionary does not contain results key\n"
-            return
-        }
-        var index = 0
-        for track in trackArray {
-            if let trackDictionary = track as? JSONDictionary,
-                let title = trackDictionary["title"] as? String,
-                let mediaURLString = trackDictionary["mediaUrl"] as? String,
-                let mediaURL = URL(string: mediaURLString),
-                let imageURLString = trackDictionary["imageUrl"] as? String,
-                let imageURL = URL(string: imageURLString) {
-                if !mediaURLString.contains("video") {
-                    let newTrack = Track(title: title, mediaURL: mediaURL, imageURL: imageURL, index: index)
-                    tracks.append(newTrack)
-                    index += 1
-                }
-            } else {
-                errorMessage += "Problem parsing trackDictionary\n"
-            }
-            
-        }
-    }
- */
 }
 
 
